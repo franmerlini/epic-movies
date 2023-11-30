19bd057206196e5fc4cbc0f7f45ce4fa0fe4772e
Francisco Merlini <merlinifrancisco@gmail.com>
2023-11-30 17:53:20 -0300

    feat: add shared-data-access-mocks lib

---
+++ libs/shared/data-access/mocks/README.md
@@ -0,0 +1,7 @@
+# shared-data-access-mocks
+
+This library was generated with [Nx](https://nx.dev).
+
+## Building
+
+Run `nx build shared-data-access-mocks` to build the library.
--- libs/shared/data-access/movies-api/src/lib/movies-api.ts
+++ libs/shared/data-access/movies-api/src/lib/movies-api.ts
@@ -1,7 +1,16 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
+import { Observable, of } from 'rxjs';
+
+import { mockedMovies } from '@epic-movies/libs/shared/assets';
+import { Movie } from '@epic-movies/libs/shared/data-access/models';
+
 @Injectable()
 export class MoviesApiService {
   constructor(private http: HttpClient) {}
+
+  getMovies(): Observable<Movie[]> {
+    return of(mockedMovies);
+  }
 }
