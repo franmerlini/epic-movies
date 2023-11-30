5898771cabe7d2fefb028e848508aa34537ceb75
Francisco Merlini <merlinifrancisco@gmail.com>
2023-11-30 15:14:57 -0300

    feat: add root store

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,17 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
+
+import { provideEffects } from '@ngrx/effects';
+import { provideStore } from '@ngrx/store';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
 
 import { shellRoutes } from '@epic-movies/shell/feature';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(shellRoutes)],
+  providers: [
+    provideStoreDevtools({ logOnly: !isDevMode() }),
+    provideEffects(),
+    provideStore(),
+    provideRouter(shellRoutes),
+  ],
 };
