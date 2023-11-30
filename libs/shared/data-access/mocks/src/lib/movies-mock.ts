4b54ba6bca287474688c68775ee5eb039b560c00
Francisco Merlini <merlinifrancisco@gmail.com>
2023-11-30 19:44:02 -0300

    refactor: add movie id

--- libs/shared/data-access/mocks/src/lib/movies-mock.ts
+++ libs/shared/data-access/mocks/src/lib/movies-mock.ts
@@ -1,5 +1,6 @@
 export const mockedMovies = [
   {
+    id: 1,
     title: 'Tenet',
     description:
       'Armed with only one word, Tenet, and fighting for the survival of the entire world, a Protagonist journeys through a twilight world of international espionage on a mission that will unfold in something beyond real time.',
@@ -10,6 +11,7 @@ export const mockedMovies = [
     trailerLink: 'https://www.youtube.com/watch?v=LdOM0x0XDMo',
   },
   {
+    id: 2,
     title: 'Spider-Man: Into the Spider-Verse',
     description:
       'Teen Miles Morales becomes the Spider-Man of his universe, and must join with five spider-powered individuals from other dimensions to stop a threat for all realities.',
@@ -20,6 +22,7 @@ export const mockedMovies = [
     trailerLink: 'https://www.youtube.com/watch?v=tg52up16eq0',
   },
   {
+    id: 3,
     title: 'Knives Out',
     description:
       'A detective investigates the death of a patriarch of an eccentric, combative family.',
@@ -30,6 +33,7 @@ export const mockedMovies = [
     trailerLink: 'https://www.youtube.com/watch?v=qGqiHJTsRkQ',
   },
   {
+    id: 4,
     title: 'Guardians of the Galaxy',
     description:
       'A group of intergalactic criminals must pull together to stop a fanatical warrior with plans to purge the universe.',
@@ -40,6 +44,7 @@ export const mockedMovies = [
     trailerLink: 'https://www.youtube.com/watch?v=d96cjJhvlMA',
   },
   {
+    id: 5,
     title: 'Avengers: Age of Ultron',
     description:
       "When Tony Stark and Bruce Banner try to jump-start a dormant peacekeeping program called Ultron, things go horribly wrong and it's up to Earth's mightiest heroes to stop the villainous Ultron from enacting his terrible plan.",
