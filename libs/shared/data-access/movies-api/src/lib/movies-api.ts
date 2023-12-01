faa8787a81ed1fa489c87bfd5e5e255516e661de
Francisco Merlini <merlinifrancisco@gmail.com>
2023-11-30 22:21:25 -0300

    refactor(shared): add movies url

--- libs/shared/data-access/movies-api/src/lib/movies-api.ts
+++ libs/shared/data-access/movies-api/src/lib/movies-api.ts
@@ -10,11 +10,16 @@ import { Movie } from '@epic-movies/libs/shared/data-access/models';
 
 @Injectable()
 export class MoviesApiService {
+  private readonly url: string;
   private appConfig = inject(APP_CONFIG);
   private readonly http = inject(HttpClient);
 
+  constructor() {
+    this.url = `${this.appConfig.baseURL}/movies`;
+  }
+
   getMovies(): Observable<Movie[]> {
-    // return this.http.get<Movie[]>(this.appConfig);
+    // return this.http.get<Movie[]>(`${this.url}`);
     return of(mockedMovies).pipe(delay(2000));
   }
 }
