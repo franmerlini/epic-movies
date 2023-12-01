7a7de95f846da6972cb66092bb22df1522537fc4
Francisco Merlini <merlinifrancisco@gmail.com>
2023-11-30 22:56:50 -0300

    feat(app): add router store

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,19 +3,27 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { provideEffects } from '@ngrx/effects';
+import { provideRouterStore } from '@ngrx/router-store';
 import { provideStore } from '@ngrx/store';
 import { provideStoreDevtools } from '@ngrx/store-devtools';
 
 import { getAppConfigProvider } from '@epic-movies/libs/shared/app-config';
 import { ShellRoutes } from '@epic-movies/shell/feature';
+
 import { environment } from '../environments/environment';
+import { ROOT_EFFECTS, ROOT_REDUCERS } from './store';
+import * as fromRouter from './store/reducers/router.reducers';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(ShellRoutes),
+    provideStore(ROOT_REDUCERS),
+    provideEffects(ROOT_EFFECTS),
+    provideRouterStore({
+      stateKey: fromRouter.routerFeatureKey,
+      serializer: fromRouter.CustomSerializer,
+    }),
     provideStoreDevtools(),
-    provideEffects(),
-    provideStore(),
     provideHttpClient(),
     getAppConfigProvider(environment),
   ],
