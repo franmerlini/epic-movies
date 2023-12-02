146b4f06809f5e57e8199819cb3cc3c397f74b8c
franmerlini <merlinifrancisco@gmail.com>
2023-12-02 20:08:26 -0300

    feat(movie): add extra selector

--- libs/home/data-access/src/lib/store/movies.state.ts
+++ libs/home/data-access/src/lib/store/movies.state.ts
@@ -1,44 +1,34 @@
 import { EntityAdapter, EntityState, createEntityAdapter } from '@ngrx/entity';
-import { createFeature, createReducer, on } from '@ngrx/store';
+import { createFeature, createReducer, createSelector, on } from '@ngrx/store';
 
+import { selectCurrentRoute } from '@epic-movies/libs/shared/store';
 import { MoviesApiActions, MoviesPageActions } from './movies.actions';
 import { MoviesEntity } from './movies.models';
 
 interface MoviesState extends EntityState<MoviesEntity> {
-  selectedId: number | null;
   loaded: boolean;
   error: string | null;
 }
 
-const moviesAdapter: EntityAdapter<MoviesEntity> =
-  createEntityAdapter<MoviesEntity>();
+const moviesAdapter: EntityAdapter<MoviesEntity> = createEntityAdapter<MoviesEntity>();
 
 const initialState: MoviesState = moviesAdapter.getInitialState({
-  selectedId: null,
   loaded: false,
   error: null,
 });
 
 const reducer = createReducer(
   initialState,
-  on(MoviesPageActions.init, (state) => ({
-    ...state,
-    loaded: false,
-    error: null,
-  })),
-  on(MoviesApiActions.loadMoviesSuccess, (state, { movies }) =>
-    moviesAdapter.setAll(movies, { ...state, loaded: true })
-  ),
-  on(MoviesApiActions.loadMoviesFailure, (state, { error }) => ({
-    ...state,
-    error,
-  }))
+  on(MoviesPageActions.init, (state) => ({ ...state, loaded: false, error: null })),
+  on(MoviesApiActions.loadMoviesSuccess, (state, { movies }) => moviesAdapter.setAll(movies, { ...state, loaded: true })),
+  on(MoviesApiActions.loadMoviesFailure, (state, { error }) => ({ ...state, error }))
 );
 
 export const MoviesFeature = createFeature({
   name: 'moviesFeature',
   reducer,
-  extraSelectors: ({ selectMoviesFeatureState }) => ({
+  extraSelectors: ({ selectMoviesFeatureState, selectEntities }) => ({
     ...moviesAdapter.getSelectors(selectMoviesFeatureState),
+    selectSelectedMovie: createSelector(selectEntities, selectCurrentRoute, (entities, { params }) => entities[params['id']]),
   }),
 });
