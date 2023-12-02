aaaea35ccc75f0878af8d43538146df465c4a1bc
franmerlini <merlinifrancisco@gmail.com>
2023-12-02 20:47:07 -0300

    feat(shell): add header logo redirect

--- libs/shell/ui/header/src/lib/header.component.ts
+++ libs/shell/ui/header/src/lib/header.component.ts
@@ -1,10 +1,10 @@
-import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'lib-header',
   standalone: true,
-  imports: [CommonModule],
+  imports: [RouterLink],
   templateUrl: './header.component.html',
   styles: [],
   changeDetection: ChangeDetectionStrategy.OnPush,
