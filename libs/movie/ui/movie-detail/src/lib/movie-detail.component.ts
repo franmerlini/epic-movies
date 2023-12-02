8fefc82b39dfa37544f2d09a8d6754e1d3b61996
franmerlini <merlinifrancisco@gmail.com>
2023-12-02 20:09:17 -0300

    feat(movie): add movie detail content

--- libs/movie/ui/movie-detail/src/lib/movie-detail.component.ts
+++ libs/movie/ui/movie-detail/src/lib/movie-detail.component.ts
@@ -1,12 +1,23 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, OnChanges } from '@angular/core';
+
+import { Movie } from '@epic-movies/libs/shared/data-access/models';
 
 @Component({
   selector: 'lib-movie-detail',
   standalone: true,
   imports: [CommonModule],
-  template: `<p>movies-ui-movie-detail works!</p>`,
-  styles: [],
+  templateUrl: './movie-detail.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class MovieDetailComponent {}
+export class MovieDetailComponent implements OnChanges {
+  @Input({ required: true }) movie: Movie | undefined | null;
+
+  imageUrl: string | undefined;
+
+  ngOnChanges(): void {
+    if (this.movie?.image) {
+      this.imageUrl = `../../../../../../../../assets/images/${this.movie.image}.png`;
+    }
+  }
+}
