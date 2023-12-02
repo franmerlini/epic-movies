c19b0efb683327b4bed69b0bdd6326b558de370d
franmerlini <merlinifrancisco@gmail.com>
2023-12-02 20:09:32 -0300

    refactor(movie): remove console.log

--- libs/movie/utils/src/guards/exists-movie.guard.ts
+++ libs/movie/utils/src/guards/exists-movie.guard.ts
@@ -9,18 +9,16 @@ import { Store } from '@ngrx/store';
 import { MoviesFeature, MoviesPageActions } from '@epic-movies/libs/home/data-access';
 import { Movie } from '@epic-movies/libs/shared/data-access/models';
 
-export const ExistsMovieGuard = (): CanActivateFn => {
+export const existsMovieGuard = (): CanActivateFn => (route: ActivatedRouteSnapshot) => {
   const store = inject(Store);
 
-  return (route: ActivatedRouteSnapshot) => {
-    return checkStore(store).pipe(
-      switchMap(() => {
-        const id = +route.params['id'];
-        return hasMovie(id, store);
-      }),
-      catchError(() => of(false))
-    );
-  };
+  return checkStore(store).pipe(
+    switchMap(() => {
+      const id = +route.params['id'];
+      return hasMovie(id, store);
+    }),
+    catchError(() => of(false))
+  );
 };
 
 const checkStore = (store: Store): Observable<boolean> => {
@@ -39,7 +37,6 @@ const hasMovie = (id: number, store: Store): Observable<boolean> => {
   return store.select(MoviesFeature.selectEntities).pipe(
     map((entities: Dictionary<Movie>) => {
       const movie = entities[id];
-      console.log(movie);
       return !!movie;
     }),
     take(1)
